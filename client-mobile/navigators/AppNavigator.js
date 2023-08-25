bb6488831921bfc05446b53038cea2aee3007c90
Vania <vnmrnda@gmail.com>
2023-08-25 18:22:48 +0700

    feat: location marker

--- client-mobile/navigators/AppNavigator.js
+++ client-mobile/navigators/AppNavigator.js
@@ -1,11 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStackNavigator from "./AuthStackNavigator";
-
+import MapsScreen from "../screens/MapsScreen";
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <AuthStackNavigator/>
+      {/* <AuthStackNavigator/> */}
+      <MapsScreen />
       {/* jika ada penambahan Navigator baru ( tabs / drawer / dll), letakkan di sini yaaa */}
     </NavigationContainer>
   )
