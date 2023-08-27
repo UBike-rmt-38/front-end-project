1cd225e8de59e6bbe1ac4e0e5c0db412be5911c6
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-28 00:12:15 +0700

    chore: bug on animated

--- client-mobile/components/MapsPin.js
+++ client-mobile/components/MapsPin.js
@@ -2,6 +2,6 @@ import React from "react";
 import { Marker } from "react-native-maps";
 const UBikeMapPin = require("../assets/UBike-map-pin.png")
 
-export default function MapsPin({ coordinate, title }) {
-  return <Marker coordinate={coordinate} title={title} image={UBikeMapPin} />;
+export default function MapsPin({ coordinate, title, description, onPress }) {
+  return <Marker coordinate={coordinate} title={title} description={description} image={UBikeMapPin} onPress={onPress} />;
 }
--- client-mobile/screens/MapsScreen.js
+++ client-mobile/screens/MapsScreen.js
@@ -9,7 +9,7 @@ import {
   Modal,
   TouchableHighlight,
 } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import MapsPin from "../components/MapsPin";
 import Scanner from "../components/Scanner";
 import * as Location from "expo-location";
@@ -20,6 +20,22 @@ import { setIsSignedIn } from "../stores/reducers/authSlice";
 import { useQuery } from "@apollo/client";
 import { GET_STATIONS } from "../constants/query";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+
+const startLatLng = {
+  latitude: 0,
+  longitude: 0,
+};
+
+const endLatLng = {
+  latitude: 0,
+  longitude: 0,
+};
 
 export default function MapsScreen() {
   const [stations, setStations] = useState([]);
@@ -29,6 +45,54 @@ export default function MapsScreen() {
   const [nearestStations, setNearestStations] = useState([]);
   const mapRef = useRef(null);
   // const navigation = useNavigation();
+  const [userLocation, setUserLocation] = useState(null);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [selectedStation, setSelectedStation] = useState(null);
+
+  const openConfirmationModal = (station) => {
+    setSelectedStation(station);
+    setShowConfirmationModal(true);
+  };
+
+  const closeConfirmationModal = () => {
+    setShowConfirmationModal(false);
+    setSelectedStation(null);
+  };
+
+  const animationValue = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    const interpolatedLatitude =
+      startLatLng.latitude +
+      animationValue.value * (endLatLng.latitude - startLatLng.latitude);
+    const interpolatedLongitude =
+      startLatLng.longitude +
+      animationValue.value * (endLatLng.longitude - startLatLng.longitude);
+
+    return {
+      latitude: interpolatedLatitude,
+      longitude: interpolatedLongitude,
+    };
+  });
+
+  const moveToSelectedStation = () => {
+    const { latitude, longitude } = selectedStation;
+    console.log(latitude, longitude, "<<< selected");
+    const duration = 5000;
+    startLatLng.latitude = userLocation.latitude;
+    startLatLng.longitude = userLocation.longitude;
+    endLatLng.latitude = latitude;
+    endLatLng.longitude = longitude;
+
+    // animationValue.value = withTiming(0, { duration, easing: Easing.linear });
+
+    // Trigger animation
+    animationValue.value = withTiming(1, {
+      duration,
+      easing: Easing.linear,
+      useNativeDriver: false,
+    });
+  };
 
   const { data, loading, error } = useQuery(GET_STATIONS, {
     onCompleted: (data) => {
@@ -86,13 +150,16 @@ export default function MapsScreen() {
     setNearestStations(sortedStations);
   };
 
-  const userLocation = async () => {
+  const getUserLocation = async () => {
     try {
       const { status } = await Location.requestForegroundPermissionsAsync();
-
       if (status === "granted") {
         const location = await Location.getCurrentPositionAsync();
         const { latitude, longitude } = location.coords;
+        const heading = location.coords.heading;
+        console.log(heading);
+
+        setUserLocation({ latitude, longitude, heading });
 
         updateNearestStations(latitude, longitude);
 
@@ -144,14 +211,30 @@ export default function MapsScreen() {
           onChangeText={(text) => setSearch(text)}
           onFocus={() => setShowFlatList(true)}
         />
-        {/* <TouchableOpacity style={styles.locationButton} onPress={userLocation}>
+        {/* <TouchableOpacity style={styles.locationButton} onPress={getUserLocation}>
           <Text style={styles.buttonText}>Go to My Location</Text>
         </TouchableOpacity> */}
       </View>
       <TouchableHighlight
-        style={{ height: 50, width: 50, zIndex: 1, position: "absolute", right: 20, top: 140, backgroundColor: "white", borderRadius: 90, alignItems: "center", justifyContent: "center" }}
+        style={{
+          height: 50,
+          width: 50,
+          zIndex: 1,
+          position: "absolute",
+          right: 20,
+          top: 140,
+          backgroundColor: "white",
+          borderRadius: 90,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-        <Ionicons onPress={userLocation} name="locate" size={40} color="skyblue" />
+        <Ionicons
+          onPress={getUserLocation}
+          name="locate"
+          size={40}
+          color="skyblue"
+        />
       </TouchableHighlight>
 
       {showFlatList && (
@@ -191,8 +274,24 @@ export default function MapsScreen() {
               longitude: station.longitude,
             }}
             title={station.name}
+            description={station.address}
+            onPress={() => openConfirmationModal(station)}
           />
         ))}
+        {userLocation && (
+          <Marker coordinate={userLocation} title="My Location">
+            <View>
+              <Ionicons
+                style={{
+                  transform: [{ rotate: `${userLocation.heading}deg` }],
+                }}
+                name="navigate-circle-outline"
+                size={40}
+                color="blue"
+              />
+            </View>
+          </Marker>
+        )}
       </MapView>
 
       <TouchableOpacity style={styles.button} onPress={openScanner}>
@@ -208,6 +307,29 @@ export default function MapsScreen() {
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
         <Text style={styles.button}>Logout</Text>
       </TouchableOpacity>
+      <Modal
+        visible={showConfirmationModal}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Move to {selectedStation?.name}?</Text>
+            <TouchableOpacity
+              style={styles.modalButtonYes}
+              onPress={moveToSelectedStation}
+            >
+              <Text>Yes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalButtonNo}
+              onPress={closeConfirmationModal}
+            >
+              <Text>No</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -275,5 +397,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#006241",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalButtonYes: {
+    marginTop: 10,
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalButtonNo: {
+    marginTop: 10,
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius: 5,
   },
 });
