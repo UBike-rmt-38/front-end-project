3408561163926ce71e350f9e4eea66cc8d5d7af7
Vania <vnmrnda@gmail.com>
2023-08-28 22:36:36 +0700

    feat: form add bikes

--- client-admin/src/components/BicycleForm.jsx
+++ client-admin/src/components/BicycleForm.jsx
@@ -1,72 +1,210 @@
-import React from 'react'
+import { useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import { ADD_BICYCLE, GET_CATEGORIES, GET_STATIONS } from "../constants/query";
+
+
 
 export default function BicycleForm() {
+  const bicycle = {
+    name: "",
+    feature: "",
+    imageUrl: "",
+    description: "",
+    price: 0,
+    stationId: "",
+    categoryId: "",
+  };
+
+  const [input, setInput] = useState(bicycle);
+  const [addBicycle] = useMutation(ADD_BICYCLE);
+  const { loading, error, data } = useQuery(GET_CATEGORIES);
+  const {
+    loading: stationLoading,
+    error: stationError,
+    data: stationData,
+  } = useQuery(GET_STATIONS);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log(input);
+      const { data, errors } = await addBicycle({
+        variables: input,
+      });
+      console.log("Bicycle added:", data);
+      console.log(errors);
+      setInput(bicycle);
+    } catch (error) {
+      console.error("Error adding bicycle:", error);
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} className="p-10 bg-white rounded shadow-xl">
-      <h1 className="w-full text-3xl text-black pb-6">Add Station</h1>
-      <div className="grid md:grid-cols-2 md:gap-6">
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            onChange={onChangeInput}
-            value={input.name}
-            type="text"
-            name="name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
-            placeholder=" "
-            required
-          />
-          <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            Station Name
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            onChange={onChangeInput}
-            value={input.address}
-            type="text"
-            name="address"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
-            placeholder=" "
-            required
-          />
-          <label htmlFor="address" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            Address
-          </label>
-        </div>
+      <h1 className="w-full text-3xl text-black pb-6">Add Bicycle</h1>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          onChange={(event) => setInput({ ...input, name: event.target.value })}
+          value={input.name || ""}
+          type="text"
+          name="name"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="name"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Name
+        </label>
       </div>
-      <div className="grid md:grid-cols-2 md:gap-6">
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            onChange={onChangeInput}
-            value={input.latitude}
-            type="text"
-            name="latitude"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
-            placeholder=" "
-            required
-          />
-          <label htmlFor="latitude" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            Latitude
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            onChange={onChangeInput}
-            value={input.longitude}
-            type="text"
-            name="longitude"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
-            placeholder=" "
-            required
-          />
-          <label htmlFor="longitude" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-            Longitude
-          </label>
-        </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          onChange={(event) =>
+            setInput({ ...input, feature: event.target.value })
+          }
+          value={input.feature || ""}
+          type="text"
+          name="feature"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="feature"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Feature
+        </label>
       </div>
-      <button type="submit" className="...">
-        Add Station
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          onChange={(event) =>
+            setInput({ ...input, imageUrl: event.target.value })
+          }
+          value={input.imageUrl || ""}
+          type="text"
+          name="imageUrl"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="imageUrl"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Image URL
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          onChange={(event) =>
+            setInput({ ...input, description: event.target.value })
+          }
+          value={input.description || ""}
+          type="text"
+          name="description"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="description"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Description
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          onChange={(event) =>
+            setInput({ ...input, price: parseInt(event.target.value) })
+          }
+          value={input.price}
+          type="number"
+          name="price"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="price"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Price
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <label
+          htmlFor="stationId"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Station:
+        </label>
+        <select
+          id="stationId"
+          name="stationId"
+          onChange={(event) =>
+            setInput({ ...input, stationId: parseInt(event.target.value) })
+          }
+          value={input.stationId || ""}
+          className="block w-full mt-1 py-2.5 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          required
+        >
+          <option value="">Select a station</option>
+          {stationLoading ? (
+            <option>Loading stations...</option>
+          ) : stationError ? (
+            <option>Error loading stations</option>
+          ) : (
+            stationData.getStations.map((station) => (
+              <option key={station.id} value={station.id}>
+                {station.name}
+              </option>
+            ))
+          )}
+        </select>
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <label
+          htmlFor="categoryId"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Category:
+        </label>
+        <select
+          id="categoryId"
+          name="categoryId"
+          onChange={(event) =>
+            setInput({ ...input, categoryId: parseInt(event.target.value) })
+          }
+          value={input.categoryId || ""}
+          className="block w-full mt-1 py-2.5 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-600 peer"
+          required
+        >
+          <option value="" disabled>
+            Select a category
+          </option>
+          {loading ? (
+            <option>Loading categories...</option>
+          ) : error ? (
+            <option>Error loading categories</option>
+          ) : (
+            data.getCategories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))
+          )}
+        </select>
+      </div>
+      <button
+        type="submit"
+        className="w-40 mb-8 bg-white text-teal-600 font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center"
+      >
+        Add Bicycle
       </button>
     </form>
-  )
+  );
 }
