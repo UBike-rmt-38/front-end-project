35c4ebe1f64313d43201255fd2353e7b217d7a35
Vania <vnmrnda@gmail.com>
2023-08-30 00:25:10 +0700

    chore: wiring home screen

--- client-mobile/navigators/AppNavigator.js
+++ client-mobile/navigators/AppNavigator.js
@@ -1,18 +1,10 @@
-import { NavigationContainer } from "@react-navigation/native";
 import AuthStackNavigator from "./AuthStackNavigator";
-import HistoryScreen from "../screens/HistoryScreen";
-import HomeScreen from "../screens/HomeScreen";
-import LoginScreen from "../screens/LoginScreen";
-import TransactionScreen from "../screens/TransactionScreen";
-import MapsScreen from "../screens/MapsScreen";
-
+import { NavigationContainer } from "@react-navigation/native";
+NavigationContainer
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      {/* <AuthStackNavigator/> */}
-      {/* <HistoryScreen /> */}
-      <MapsScreen />
-      {/* <TransactionScreen /> */}
+      <AuthStackNavigator/>
       {/* jika ada penambahan Navigator baru ( tabs / drawer / dll), letakkan di sini yaaa */}
     </NavigationContainer>
   );
