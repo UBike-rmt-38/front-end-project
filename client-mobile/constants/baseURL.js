34e101b5cd9f98bf03e642aa017f1a9871fde80b
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-30 20:49:57 +0700

    chore: to pull

--- client-mobile/constants/baseURL.js
+++ client-mobile/constants/baseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://abde-111-94-77-81.ngrok-free.app";
+export const baseURL = "https://698b-202-46-68-73.ngrok-free.app";
 
 export const ROUTES_API =
   "https://routes.googleapis.com/directions/v2:computeRoutes";
