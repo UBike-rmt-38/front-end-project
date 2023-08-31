3cd635eab2df8b544b3d788744197bdbc633bb59
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-31 13:27:02 +0700

    chore: to rebase dev

--- client-mobile/constants/baseURL.js
+++ client-mobile/constants/baseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://698b-202-46-68-73.ngrok-free.app";
+export const baseURL = "https://5ec1-202-46-68-117.ngrok-free.app";
 
 export const ROUTES_API =
   "https://routes.googleapis.com/directions/v2:computeRoutes";
